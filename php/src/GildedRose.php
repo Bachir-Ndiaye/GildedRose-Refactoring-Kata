7800d85938831bab68771018a5cd041ee92c8adb
NDIAYE <am.ndiaye@cgi.com>
2026-07-06 11:00:38 +0200

    extract logic in different functions

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -14,25 +14,28 @@ final class GildedRose
     ) {
     }
     const MAX_QUALITY = 50;
+    const MIN_QUALITY = 0;
     const ZERO = 0;
-    
-    public function increaseQuality(Item $item): void
+
+    private function increaseQuality(Item $item): void
     {
         if($item->quality < self::MAX_QUALITY) {
             $item->quality++;
         }
     }
 
-    public function decreaseQuality(Item $item): void
+    private function decreaseQuality(Item $item): void
     {
-        $item->quality--;
+        if($item->quality > self::MIN_QUALITY) {
+            $item->quality--;
+        }
     }
 
-    public function increaseSellIn(Item $item): void
+    private function increaseSellIn(Item $item): void
     {
         $item->sellIn++;
     }
-    public function decreaseSellIn(Item $item): void
+    private function decreaseSellIn(Item $item): void
     {
         $item->sellIn--;
     }
@@ -40,51 +43,67 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != EnumItem::AGED_BRIE->value and $item->name != EnumItem::BACKSTAGE->value) {
-                if ($item->quality > self::ZERO) {
-                    if ($item->name != EnumItem::SULFURAS->value) {
-                        $this->decreaseQuality($item);
-                    }
-                }
-            } else {
-                if ($item->quality < self::MAX_QUALITY) {
-                    $this->increaseQuality($item);
-                    if ($item->name == EnumItem::BACKSTAGE->value) {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < self::MAX_QUALITY) {
-                                $this->increaseQuality($item);
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < self::MAX_QUALITY) {
-                                $this->increaseQuality($item);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != EnumItem::SULFURAS->value) {
-                $this->decreaseSellIn($item);
-            }
-
-            if ($item->sellIn < self::ZERO) {
-                if ($item->name != EnumItem::AGED_BRIE->value) {
-                    if ($item->name != EnumItem::BACKSTAGE->value) {
-                        if ($item->quality > self::ZERO) {
-                            if ($item->name != EnumItem::SULFURAS->value) {
-                                $this->decreaseQuality($item);
-                            }
-                        }
-                    } else {
-                        $item->quality = self::ZERO;
-                    }
-                } else {
-                    if ($item->quality < self::MAX_QUALITY) {
-                        $this->increaseQuality($item);
-                    }
-                }
+            switch ($item->name){
+                case EnumItem::AGED_BRIE->value: 
+                    $this->updateAgedBrie($item); 
+                    break;
+                case EnumItem::BACKSTAGE->value:
+                    $this->updateBackstage($item);
+                    break;
+                case EnumItem::SULFURAS->value:
+                    $this->updatedSulfuras($item);
+                    break;
+                default:
+                    $this->updateNormalItem($item);
             }
         }
+    }
+
+    private function updateNormalItem(Item $item): void
+    {
+        $this->decreaseQuality($item);
+        $this->decreaseSellIn($item);
+
+        if($item->sellIn < self::ZERO) {
+            $this->decreaseQuality($item);
+        }
+    }
+
+    private function updateAgedBrie(Item $item): void
+    {
+        $this->increaseQuality($item);
+        $this->decreaseSellIn($item);
+
+        if($item->sellIn < self::ZERO) {
+            $this->increaseQuality($item);
+        }
+    }
+
+    private function updateBackstage(Item $item): void
+    {
+        $this->increaseQuality($item);
+
+        if($item->sellIn <= 10) {
+            $this->increaseQuality($item);
+        }
+
+        if($item->sellIn <= 5) {
+            $this->increaseQuality($item);
+        }
+        $this->decreaseSellIn($item);
+
+        if($item->sellIn < self::ZERO) {
+            $this->setQualityToZero($item);
+        }
+    }
+
+    private function setQualityToZero(Item $item): void
+    {
+        $item->quality = self::ZERO;
+    }
+
+    private function updatedSulfuras(Item $item): void
+    {
+        // "Sulfuras" est un objet légendaire et comme tel sa qualité est de 80 et elle ne change jamais.
     }
 }
